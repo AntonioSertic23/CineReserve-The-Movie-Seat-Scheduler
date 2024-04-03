96131cfb742b535b7d773876dbcccee2976f6040
Antonio <sertic43@gmail.com>
2024-04-03 21:29:58 +0200

    Create AddTheater

--- src/js/views/view.js
+++ src/js/views/view.js
@@ -1,8 +1,13 @@
 export default class View {
   _data;
+  _theaterData;
 
   render(type) {
     this._data = type;
+    this._theaterData = [
+      { id: 1, movie: "Shrek 3", rows: 4, columns: 7 },
+      { id: 2, movie: "-", rows: 3, columns: 6 },
+    ];
     const markup = this._generateMarkup();
 
     this._clear();
